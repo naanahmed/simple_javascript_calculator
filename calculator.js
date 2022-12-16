4ed2048344ab00cfef7b752cd3ebdae4b2a7156d
Naan Ahmed <naanahmed.na@gmail.com>
2022-12-17 00:35:46 +0300

    End of Simple calculator

--- calculator.js
+++ calculator.js
@@ -10,8 +10,9 @@
             let value = e.target.dataset.num;
             screen.value += value;
         })
-    })
+    });
 
+//evaluate values
     equal.addEventListener('click', function(e){
         if (screen.value === ''){
             screen.value = "";
@@ -20,5 +21,13 @@
             let answer = eval(screen.value);
             screen.value = answer;
         }
-    })
+    });
+
+    clear.addEventListener('click', function(e){
+        screen.value = "";
+    });
+
+
+
 })();
+

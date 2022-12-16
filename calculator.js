095d58746abf3059e8b693bd2020c2ed87bbcb73
Naan Ahmed <naanahmed.na@gmail.com>
2022-12-16 23:48:12 +0300

    Initial JS code

---
+++ calculator.js
@@ -0,0 +1,24 @@
+(function(){
+    let screen = document.querySelector('.screen')
+    let buttons = document.querySelectorAll('.btn')
+    let clear = document.querySelector('.btn-clear')
+    let equal = document.querySelector('.btn-equal')
+
+//push values to screen
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    })
+
+    equal.addEventListener('click', function(e){
+        if (screen.value === ''){
+            screen.value = "";
+        }
+        else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+})();
